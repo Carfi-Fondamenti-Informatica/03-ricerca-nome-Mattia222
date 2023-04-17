66d4a24e64f3e75a0f038d3de76d1df7d5d7aff1
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-04-17 19:40:52 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main() {
+   char lista_nomi[3][5] = {"", "", ""};
+    char nome[5] = {""};
+
+    cin >> lista_nomi[0];
+    cin >> lista_nomi[1];
+    cin >> lista_nomi[2];
+    cout << endl;
+
+    cin >> nome;
+
+    if((strcmp(nome,lista_nomi[0])==0)){
+        cout << 0;
+    } else if((strcmp(nome,lista_nomi[1])==0)){
+        cout << 1;
+    }else if((strcmp(nome,lista_nomi[2])==0)){
+        cout <<2;
+    }
    return 0;
 }
